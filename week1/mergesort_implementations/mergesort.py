5b48ec829ddcbe44af093a27855002367be2c90c
Diego Dell'Era <ddellera@socialmetrix.com>
2016-07-16 13:45:48 -0300

    modified python mergesort

--- week1/mergesort_implementations/mergesort.py
+++ week1/mergesort_implementations/mergesort.py
@@ -1,13 +1,25 @@
+#!/usr/bin/python
+
 from heapq import merge
  
 def merge_sort(m):
+    print("sorting", len(m), "elements:",m)
     if len(m) <= 1:
+        print("done",m)
         return m
  
     middle = len(m) // 2
     left = m[:middle]
     right = m[middle:]
- 
+    print("splitting in", len(left), "and", len(right), "sublists:",left,right)
+    
     left = merge_sort(left)
     right = merge_sort(right)
-    return list(merge(left, right))
+    print("left:",left,"right:",right)
+
+    merged = list(merge(left, right))
+    print("merged:", merged)
+    return merged 
+
+out = merge_sort([1,7,5,4,6,8,2,3])
+print(out)
